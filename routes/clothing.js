45e7c567331e67781dbe88792525a3d9646eb947
Cameron Ford <c.r.ford@outlook.com>
2023-01-09 17:23:36 -0700

    clothing is done

---
+++ routes/clothing.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const clothingRoutes = express.Router()
+
+const clothing = [
+    {
+        name: "pants",
+        type: "clothing",
+        price: 2500,
+    },{
+        name: "shirt",
+        type: "clothing",
+        price: 800,
+    }
+]
+
+clothingRoutes.get("/", (req, res) => {
+    res.send(clothing)
+})
